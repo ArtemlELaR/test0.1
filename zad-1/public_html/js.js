7381125db3d557dd8d8ce6154eeb9904116ae3e9
Artem S <siryk.a.wezom@gmail.com>
2022-11-19 03:03:48 +0200

    zad 1-2

--- zad-1/public_html/js.js
+++ zad-1/public_html/js.js
@@ -1,3 +1,39 @@
 $(document).ready(function () {
+
+    let elemLeft, elemTop, maxElemLeft, maxElemTop, elem, elem_main;
+
+    elem = document.getElementById('container');
+    elem_main = document.getElementById('main_block');
+    maxElemLeft = document.documentElement.clientWidth - elem.offsetWidth;
+    maxElemTop = document.documentElement.clientHeight - elem.offsetHeight;
+
+
+    elem_main.addEventListener('mousemove', (e) => {
+
+        let elemXY = getCoords(elem);
+
+        if (e.clientX <= maxElemLeft) {
+ 
+            elem.style.left = (maxElemLeft - e.clientX) + 'px';
+        }
+        if (e.layerY <= maxElemTop) {
+
+            elem.style.top = (maxElemTop - e.clientY) + 'px';
+        }
+
+
+    });
+
+  
+    function getCoords(elem) {
+        let box = elem.getBoundingClientRect();
+      
+        return {
+          top: box.top + window.pageYOffset,
+          right: box.right + window.pageXOffset,
+          bottom: box.bottom + window.pageYOffset,
+          left: box.left + window.pageXOffset
+        };
+      }
     
 });
--- zad-2/public_html/js.js
+++ zad-2/public_html/js.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-   
+    var timerId;
+    $('.container input[type="checkbox"]').change(function(){
+        if(this.checked){
+            $('body button').css('opacity','0.5');
+            if(typeof timerId !== 'undefined') {
+                clearTimeout(timerId) 
+            }
+            timerId = setTimeout( function() {
+                $('body button').removeAttr('style');
+            },5000);
+        }
+    });
 });
